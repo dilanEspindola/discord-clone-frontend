0206028d1976ecd530db5aca9e6637c0ba777a4e
dilanEspindola <dilanespindola30@gmail.com>
2023-12-23 03:05:29 -0500

    replacing text

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ const Login = () => {
   return (
     <div className="bg-[url('/images/background-login.webp')] h-screen w-full bg-no-repeat bg-cover flex justify-center items-center">
       <div className=" bg-black-2 rounded-md px-16 py-10 shadow-2xl w-[500px] max-w-[500px] flex flex-col items-center">
-        <h2 className="text-2xl text-white">Hi again!</h2>
+        <h2 className="text-2xl text-white">Hi again!!</h2>
         <p className="text-gray-400 text-lg">{`We're are happy to see you!`}</p>
       </div>
     </div>
