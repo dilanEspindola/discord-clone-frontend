122eb1d8b2b2ea9bfa636ec4a4bbb59a4e729fbd
dilanEspindola <dilanespindola30@gmail.com>
2024-03-31 00:50:38 -0500

    change

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,7 +5,7 @@ const Register = () => {
     <div className="bg-[url('/images/background-login.webp')] h-screen w-full bg-no-repeat bg-cover flex justify-center items-center">
       <div className=" bg-black-2 rounded-md px-10 py-6 shadow-2xl w-[500px] max-w-[500px] flex flex-col items-center">
         <h2 className="text-2xl text-white font-bold mb-5">
-          Create an account
+          Create an account.
         </h2>
         <RegisterForm />
       </div>
