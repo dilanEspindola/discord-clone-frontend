3a6e430f123720766d3f4126f8fb5980d46025fd
dilanEspindola <dilanespindola30@gmail.com>
2024-03-30 21:39:11 -0500

    removing pipeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="h-screen w-full bg-no-repeat bg-cover">
-      <h1 className="text-2xl text-red-500 uppercase">main page</h1>
+      <h1 className="text-2xl text-red-500 uppercase">page</h1>
     </div>
   );
 };
